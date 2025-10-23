05735cce5c4c610c00970437e5d943b79d25e564
thevoxium <thevoxium@gmail.com>
2025-10-23 15:30:33 +0530

    update

--- day1.cu
+++ day1.cu
@@ -2,32 +2,35 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__global__ void hello(){
-  int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < 10) {  
+__global__ void hello() {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < 10) {  
         printf("Block Id: %d, Thread Id: %d, Global Idx: %d\n",
                blockIdx.x, threadIdx.x, idx);
     }
 }
 
-int main(){
-  int N = 1 << 10;
-  dim3 threadsPerBlock(256, 1, 1);
-  dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x);
+int main() {
+    int N = 1 << 10;
+    dim3 threadsPerBlock(256, 1, 1);
+    dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x);
   
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
   
-  cudaEventRecord(start);
-  hello<<<blocksPerGrid, threadsPerBlock>>>();
-  cudaEventRecord(stop);
+    cudaEventRecord(start);
+    hello<<<blocksPerGrid, threadsPerBlock>>>();
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
   
-  cudaEventSynchronize(stop);
+    float elapsed;
+    cudaEventElapsedTime(&elapsed, start, stop);
+    printf("Kernel execution time: %f ms\n", elapsed);
   
-  float elapsed;
-  cudaEventElapsedTime(&elapsed, start, stop);
-  printf("Kernel execution time: %f ms\n", elapsed);
-  
-  return 0;
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    return 0;
 }
