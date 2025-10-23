26b2bf8adc52661e6d3090da48e3d052e1ba279e
thevoxium <thevoxium@gmail.com>
2025-10-23 15:29:21 +0530

    update

--- day1.cu
+++ day1.cu
@@ -4,7 +4,7 @@
 
 __global__ void hello(){
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (idx < 10) { 
+  if (idx < 10) {  
         printf("Block Id: %d, Thread Id: %d, Global Idx: %d\n",
                blockIdx.x, threadIdx.x, idx);
     }
@@ -14,7 +14,20 @@ int main(){
   int N = 1 << 10;
   dim3 threadsPerBlock(256, 1, 1);
   dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x);
+  
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  
+  cudaEventRecord(start);
   hello<<<blocksPerGrid, threadsPerBlock>>>();
-  cudaDeviceSynchronize();
+  cudaEventRecord(stop);
+  
+  cudaEventSynchronize(stop);
+  
+  float elapsed;
+  cudaEventElapsedTime(&elapsed, start, stop);
+  printf("Kernel execution time: %f ms\n", elapsed);
+  
   return 0;
 }
